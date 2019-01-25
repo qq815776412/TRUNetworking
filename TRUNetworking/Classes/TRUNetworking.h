b63541972bbd84564df95b50e77d41d0b82fb374
qq815776412 <815776412@qq.com>
2019-01-25 18:50:17 +0800

    s

---
+++ TRUNetworking/Classes/TRUNetworking.h
@@ -0,0 +1,16 @@
+//
+//  TRUNetworking.h
+//  FBSnapshotTestCase
+//
+//  Created by gjm on 2019/1/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TRUNetworking : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRUNetworking/Classes/TRUNetworking.m
@@ -0,0 +1,12 @@
+//
+//  TRUNetworking.m
+//  FBSnapshotTestCase
+//
+//  Created by gjm on 2019/1/25.
+//
+
+#import "TRUNetworking.h"
+
+@implementation TRUNetworking
+
+@end
